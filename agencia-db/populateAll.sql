cfa03646f772e365934a73c1b9c28f8fc6a2a23c
mtuehara <39523096+mtuehara@users.noreply.github.com>
2018-12-12 23:52:28 -0200

    populado

--- agencia-db/populateAll.sql
+++ agencia-db/populateAll.sql
@@ -2,34 +2,249 @@
 INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
 VALUES (1, "259.731.925-39", "Leonardo Benjamin Joaquim Martins", "50.050.217-1", "(86) 99729-4529", "leonardo81@supercarioca.com", "Rua Dez", "2008-10-10");
 
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (2, "644.132.250-15", "Miguel Araujo Alves", "18.802.112-7", "(11) 5758-2982", "miguelaraujoalves@armyspy.com", "Rua Odete Carvalho Caielli", "2009-11-16");
+
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (3, "012.229.920-51", "Rafael Silva Azevedo", "37.256.163-9", "(11) 7164-6674", "RafaelSilvaAzevedo@jourrapide.com", "Praça Carlos Drumond de Andrade", "2013-11-22");
+
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (4, "079.665.160-40", "Victor Barbosa Correia", "17.742.657-3", "(71) 2651-8434", "VictorBarbosaCorreia@teleworm.us", "Travessa Evangelista Sampaio Seixas", "2014-04-12");
+
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (5, "033.724.720-04", "Sophia Pinto Correia", "30.038.278-9", "(43) 7150-3881", "SophiaPintoCorreia@armyspy.com", "Rua Senador Souza Naves", "2014-06-10");
+
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (6, "184.705.990-20", "Estevan Azevedo Barbosa", "33.313.837-5", "(17) 7579-2523", "EstevanAzevedoBarbosa@rhyta.com", "Rua Alfredo Rodrigues Simões", "2005-12-30");
+
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (7, "327.420.120-01", "Carolina Barbosa Gomes", "24.747.693-6", "(71) 5677-5302", "CarolinaBarbosaGomes@teleworm.us", "Rua São Lázaro", "2010-10-10");
+
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (8, "587.234.970-09", "Samuel Almeida Cavalcanti", "47.912.165-5", "(34) 5905-6437", "SamuelAlmeidaCavalcanti@teleworm.us", "Rua Enilza Correa", "2013-08-10");
+
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (9, "878.883.410-71", "Sophia Sousa Cavalcanti", "16.566.263-3", "(91) 4869-6470", "SophiaSousaCavalcanti@dayrep.com", "Praça Magalhães", "2017-3-23");
+
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (10, "071.753.910-56", "Evelyn Sousa Costa", "11.428.509-3", "(62) 7900-7919", "EvelynSousaCosta@jourrapide.com", "Rua João Dias", "2018-10-10");
+
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (11, "504.215.340-36", "Aline Gomes Rocha", "32.203.108-4", "(11) 7142-5017", "AlineGomesRocha@rhyta.com", "Rua Centro Africana", "2014-04-15");
+
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (12, "811.096.770-14", "André Azevedo Pereira", "40.269.900-2", "(82) 3718-4653", "AndreAzevedoPereira@dayrep.com", "Travessa Santa Ana", "2018-7-21");
+
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (13, "707.890.600-43", "Isabela Fernandes Almeida", "45.342.884-8", "(27) 4450-8496", "IsabelaFernandesAlmeida@dayrep.com", "Escada Alexandre Rodrigues", "2015-10-13");
+
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (14, "800.365.820-93", "Luan Lima Alves", "50.997.789-3", "(34) 8382-9906", "LuanLimaAlves@armyspy.com", "Rua Wilson Cunha", "2008-10-31");
+
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (15, "825.593.730-76", "Thaís Souza Pinto", "38.313.347-6", "(19) 2984-4347", "ThaisSouzaPinto@dayrep.com", "Rua Ângela Ferraro Menegaldo", "2008-10-14");
+
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (16, "968.211.030-03", "Nicolash Martins Barbosa", "46.513.286-8", "(96) 6944-8661", "NicolashMartinsBarbosa@dayrep.com", "Avenida Rio Grande do Norte", "2008-12-10");
+
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (17, "087.714.560-15", "Davi Azevedo Sousa", "38.122.799-6", "(54) 4431-5827", "DaviAzevedoSousa@jourrapide.com", "Rua Ivone Dalla Santa Zatti", "2015-10-10");
+
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (18, "669.709.730-26", "Evelyn Pinto Lima", "15.573.629-2", "(11) 4022-5594", "EvelynPintoLima@armyspy.com", "Praça Estelita Felex de Lucena", "2018-11-11");
+
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (19, "207.501.130-30", "Manuela Barros Cavalcanti", "42.126.330-1", "(61) 9286-2545", "ManuelaBarrosCavalcanti@armyspy.com", "Quadra QNP 18 Conjunto A", "2012-10-10");
+
+INSERT INTO ClienteDB (IdCliente, CPF, Nome, RG, Telefone, Email, Endereco, DataPrimeiraCompra)
+VALUES (20, "337.041.500-37", "Luan Alves Santos", "33.179.542-5", "(31) 6812-3091", "LuanAlvesSantos@dayrep.com", "Rua Palma", "2017-1-10");
+
 -- FUNCIONARIO
 INSERT INTO FuncionarioDB (IdFuncionario, CPF, Nome, RG, Telefone, Email, Endereco, Salario, Cargo, Periodo, DataAdmissao)
-VALUES (1, "805.071.302-43", "Aurora S�nia Teixeira", "18.633.472-2", "(85) 3532-7207", "aurorasoniateixeira@danzarin.com.br", "Avenida Bibio Frota", 1500.00, "Gerente", "Diurno", "2015-08-10");
+VALUES (1, "805.071.302-43", "Aurora Sônia Teixeira", "18.633.472-2", "(85) 3532-7207", "aurorasoniateixeira@danzarin.com.br", "Avenida Bibio Frota", 1500.00, "Gerente", "Diurno", "2015-08-10");
+
+INSERT INTO FuncionarioDB (IdFuncionario, CPF, Nome, RG, Telefone, Email, Endereco, Salario, Cargo, Periodo, DataAdmissao)
+VALUES (2, "709.104.094-55", "Giovanna Rocha Fernandes", "24.701.750-4", "(11) 6942-2940", "GiovannaRochaFernandes@armyspy.com", "Rua Lúcio Petit da Silva", 1000.00, "Vendedora", "Diurno", "2016-07-11");
+
+INSERT INTO FuncionarioDB (IdFuncionario, CPF, Nome, RG, Telefone, Email, Endereco, Salario, Cargo, Periodo, DataAdmissao)
+VALUES (3, "123.742.531-04", "Lucas Alves Martins", "29.166.663-2", "(11) 6602-3261", "LucasAlvesMartins@armyspy.com", "Rua Araranguá", 1700.00, "Gerente", "Diurno", "2018-08-10");
+
+INSERT INTO FuncionarioDB (IdFuncionario, CPF, Nome, RG, Telefone, Email, Endereco, Salario, Cargo, Periodo, DataAdmissao)
+VALUES (4, "477.700.821-53", "Kauê Cavalcanti Santos", "40.133.487-9", "(31) 3945-5747", "KaueCavalcantiSantos@teleworm.us", "Rua Flor-de-Antúrio", 2500.00, "Gerente", "Diurno", "2012-08-15");
+
+INSERT INTO FuncionarioDB (IdFuncionario, CPF, Nome, RG, Telefone, Email, Endereco, Salario, Cargo, Periodo, DataAdmissao)
+VALUES (5, "404.241.840-60", "Paulo Martins Cardoso", "39.050.510-9", "(75) 5422-5713", "aurorasoniateixeira@danzarin.com.br", "Rua Guacu", 2500.00, "Vendedor", "Diurno", "2016-08-10");
+
+INSERT INTO FuncionarioDB (IdFuncionario, CPF, Nome, RG, Telefone, Email, Endereco, Salario, Cargo, Periodo, DataAdmissao)
+VALUES (6, "316.304.766-17", "Daniel Pereira Carvalho", "46.147.488-8", "(96) 9953-3812", "DanielPereiraCarvalho@teleworm.us", "Rua Brasília", 3500.00, "Gerente", "Diurno", "2005-08-10");
+
 
 -- PRESTADOR SERVICO 
 INSERT INTO PrestadorServicoDB (CNPJ, Nome, Endereco, Telefone, DataContratacao, IdFuncionario)
 VALUES ("05.249.838/0001-37", "Raimundo e Larissa Ltda", "Rua Projetada 04", "(67) 2930-2347", "2018-10-12", 1);
 
+INSERT INTO PrestadorServicoDB (CNPJ, Nome, Endereco, Telefone, DataContratacao, IdFuncionario)
+VALUES ("44.496.208/0001-19", "Grupo 08 Ltda", "Rua Ministro Calógeras", "(82) 2704-2258", "2018-11-12", 2);
+
+INSERT INTO PrestadorServicoDB (CNPJ, Nome, Endereco, Telefone, DataContratacao, IdFuncionario)
+VALUES ("79.477.932/0001-80", "Thor Ltda", "Rua São Tiago", "(17) 3972-3243", "2018-12-12", 2);
+
+INSERT INTO PrestadorServicoDB (CNPJ, Nome, Endereco, Telefone, DataContratacao, IdFuncionario)
+VALUES ("43.675.636/0001-46", "Abacaxi Limpeza Ltda", "Rua Bom Jesus", "(61) 3710-0135", "2018-09-12", 2);
+
+INSERT INTO PrestadorServicoDB (CNPJ, Nome, Endereco, Telefone, DataContratacao, IdFuncionario)
+VALUES ("39.047.781/0001-05", "Fran Ltda", "Praça Vereador Raimundo Tavares", "(81) 3792-2296", "2018-06-12", 6);
+
 -- SERVICO 
 INSERT INTO ServicoDB (IdServico, Tipo, Descricao)
 VALUES (1, "Limpeza", "Apenas limpa os vidros");
+
+INSERT INTO ServicoDB (IdServico, Tipo, Descricao)
+VALUES (2, "Mecânico", "Manutenção do motor");
+
+INSERT INTO ServicoDB (IdServico, Tipo, Descricao)
+VALUES (3, "Refrigeração", "Manutenção do ar condicionado");
+
+INSERT INTO ServicoDB (IdServico, Tipo, Descricao)
+VALUES (4, "Limpeza", "Limpeza geral");
+
+INSERT INTO ServicoDB (IdServico, Tipo, Descricao)
+VALUES (5, "Manutenção", "Manutenção elétrica");
 
 -- PRESTA 
 INSERT INTO PrestaDB (IdServico, CNPJ)
 VALUES (1, "05.249.838/0001-37");
 
+INSERT INTO PrestaDB (IdServico, CNPJ)
+VALUES (2, "44.496.208/0001-19");
+
+INSERT INTO PrestaDB (IdServico, CNPJ)
+VALUES (3, "39.047.781/0001-05");
+
+INSERT INTO PrestaDB (IdServico, CNPJ)
+VALUES (4, "43.675.636/0001-46");
+
+INSERT INTO PrestaDB (IdServico, CNPJ)
+VALUES (5, "79.477.932/0001-80");
+
 -- CARRO 
 INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
 VALUES ("46619734653", "EFFA", "ULC PICAPE CD 1.0 8V 4p", "Preto", "2010", 4, "Novo");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("32795740296", "Pontiac", "Trans-Sport SE 3.1", "Preto", "1991", 4, "Usado");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("84631790455", "ASTON MARTIN", "Rapide S 6.0 V12 550cv", "Prata", "2013", 4, "Novo");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("95446361824", "TAC", "Stark 2.3 4WD 127cv TDI Diesel 3p", "Preto", "2010", 3, "Novo");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("84743284071", "Mercury", "Mystique GS 2.5 V6 Mec.", "Prata", "1995", 4, "Novo");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("06866370756", "Rolls-Royce", "Ghost ll 6.6 V12 Aut.", "Prata", "2015", 4, "Novo");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("60443388125", "RELY", "LINK 1.3 16V 5p", "Marrom", "2014", 5, "Usado");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("16604252028", "Jeep", "Grand Cherokee Laredo 2.7 I-5 TB Dies.", "Bege", "2002", 4, "Novo");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("36042351740", "Audi", "TT Roadster 2.0 16v TFSI S-Tronic", "Branco", "2007", 4, "Novo");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("59336823550", "Nissan", "Altima SE 2.4 16V", "Laranja", "1993", 4, "Usado");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("19511317787", "Porsche", "911 Carrera Coupe 3.0 370cv", "Preto", "2017", 4, "Novo");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("90971803213", "GEELY", "EC7 1.8 16V 130cv 4p Mec.", "Dourado", "2014", 4, "Novo");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("79066683810", "Rolls-Royce", "Phantom 6.7 V12 Aut.", "Dourado", "2013", 4, "Usado");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("77994795210", "Agrale", "MARRU", "Marrom", "2007", 2, "Usado");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("07920277206", "VolksWagen", "Passat VR6 2.8", "Verde", "1991", 4, "Usado");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("66616458447", "BMW", "650iA 4.8 V8 32V 360cv", "Verde", "2006", 4, "Usado");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("84482947200", "Cadillac", "Deville/Eldorado 4.9", "Amarelo", "1991", 4, "Usado");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("90568126942", "Isuzu", "ULC PICAPE CD 1.0 8V 4p", "Branco", "1995", 4, "Usado");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("83981763041", "CHANA", "Cargo CD 1.0 8V 53cv (Pick-Up)", "Bege", "2007", 2, "Novo");
+
+INSERT INTO CarroDB (Renavam, Marca, Modelo, Cor, Ano, NumeroPortas, Status)
+VALUES ("07479653728", "Rolls-Royce", "Ghost 6.6 V12 Aut.", "Bege", "2011", 4, "Novo");
+
 
 -- VENDA
 INSERT INTO VendaDB (Renavam, DataVenda, IdCliente, IdFuncionario, ValorVenda)
 VALUES ("46619734653", "2010-10-12", 1, 1, 25000.00);
 
+INSERT INTO VendaDB (Renavam, DataVenda, IdCliente, IdFuncionario, ValorVenda)
+VALUES ("19511317787", "2018-10-12", 7, 1, 125000.00);
+
+INSERT INTO VendaDB (Renavam, DataVenda, IdCliente, IdFuncionario, ValorVenda)
+VALUES ("79066683810", "2018-07-13", 8, 2, 325000.00);
+
+INSERT INTO VendaDB (Renavam, DataVenda, IdCliente, IdFuncionario, ValorVenda)
+VALUES ("07920277206", "2018-06-13", 12, 2, 35000.00);
+
+INSERT INTO VendaDB (Renavam, DataVenda, IdCliente, IdFuncionario, ValorVenda)
+VALUES ("83981763041", "2014-03-13", 20, 3, 50000.00);
+
+INSERT INTO VendaDB (Renavam, DataVenda, IdCliente, IdFuncionario, ValorVenda)
+VALUES ("90568126942", "2016-07-13", 10, 5, 40000.00);
+
+INSERT INTO VendaDB (Renavam, DataVenda, IdCliente, IdFuncionario, ValorVenda)
+VALUES ("66616458447", "2017-12-13", 10, 5, 187000.00);
+
+
+
 -- COMPRA 
 INSERT INTO CompraDB (Renavam, DataCompra, IdCliente, IdFuncionario, ValorCompra)
-VALUES ("46619734653", "2015-10-01", 1, 1, 10000.00);
+VALUES ("46619734653", "2015-10-01", 3, 1, 10000.00);
+
+INSERT INTO CompraDB (Renavam, DataCompra, IdCliente, IdFuncionario, ValorCompra)
+VALUES ("19511317787", "2015-06-19", 1, 1, 80000.00);
+
+INSERT INTO CompraDB (Renavam, DataCompra, IdCliente, IdFuncionario, ValorCompra)
+VALUES ("79066683810", "2018-12-01", 2, 6, 180000.00);
+
+INSERT INTO CompraDB (Renavam, DataCompra, IdCliente, IdFuncionario, ValorCompra)
+VALUES ("60443388125", "2015-11-01", 14, 2, 40000.00);
+
+INSERT INTO CompraDB (Renavam, DataCompra, IdCliente, IdFuncionario, ValorCompra)
+VALUES ("07920277206", "2018-10-31", 17, 4, 19000.00);
+
+INSERT INTO CompraDB (Renavam, DataCompra, IdCliente, IdFuncionario, ValorCompra)
+VALUES ("07479653728", "2018-12-01", 12, 2, 100000.00);
+
 
 -- RECEBE 
 INSERT INTO RecebeDB (Renavam, IdServico, CNPJ, DataInicio, DataFim)
 VALUES ("46619734653", 1, "05.249.838/0001-37", "2015-10-02", "2015-10-03");
+
+INSERT INTO RecebeDB (Renavam, IdServico, CNPJ, DataInicio, DataFim)
+VALUES ("19511317787", 2, "44.496.208/0001-19", "2015-10-07", "2015-10-18");
+
+INSERT INTO RecebeDB (Renavam, IdServico, CNPJ, DataInicio, DataFim)
+VALUES ("79066683810", 3, "39.047.781/0001-05", "2018-06-20", "2015-06-23");
+
+INSERT INTO RecebeDB (Renavam, IdServico, CNPJ, DataInicio, DataFim)
+VALUES ("07920277206", 4, "43.675.636/0001-46", "2018-11-02", "2018-11-03");
+
+INSERT INTO RecebeDB (Renavam, IdServico, CNPJ, DataInicio, DataFim)
+VALUES ("07479653728", 5, "79.477.932/0001-80", "2018-12-02", "2018-12-10");
