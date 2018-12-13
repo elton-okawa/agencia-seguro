dc3d538295b4442b96818afa677280ed90e68307
Elton Okawa <elton_okawa@hotmail.com>
2018-12-13 10:43:06 -0200

    finished inner join

--- agencia-seguro/src/dao/RecebeDAO.java
+++ agencia-seguro/src/dao/RecebeDAO.java
@@ -10,6 +10,10 @@ import model.Recebe;
 public class RecebeDAO extends DAO<Recebe>{
 
 	private static RecebeDAO instance = new RecebeDAO();
+	private final String tableJoin = "(((RecebeDB INNER JOIN ServicoDB ON RecebeDB.IdServico = ServicoDB.IdServico) "
+			+ "	INNER JOIN CarroDB ON RecebeDB.Renavam = CarroDB.Renavam) "
+			+ " INNER JOIN PrestadorServicoDB ON RecebeDB.CNPJ= PrestadorServicoDB.CNPJ)";
+	
 	
 	private RecebeDAO() {
 		super();
@@ -20,37 +24,37 @@ public class RecebeDAO extends DAO<Recebe>{
 	}
 	
 	public List<Recebe> getAllRecebeByRenavam (String renavam) throws SQLException {
-		String query = String.format("SELECT * FROM RecebeDB WHERE Renavam = '%s'", renavam);
+		String query = String.format("SELECT * FROM %s WHERE RecebeDB.Renavam = '%s'", tableJoin, renavam);
 		return super.getAll(query);
 	}
 	
 	public List<Recebe> getAllRecebeByIdServico (int idServico) throws SQLException {
-		String query = String.format("SELECT * FROM RecebeDB WHERE IdServico = %d", idServico);
+		String query = String.format("SELECT * FROM %s WHERE RecebeDB.IdServico = %d", tableJoin, idServico);
 		return super.getAll(query);
 	}
 	
 	public List<Recebe> getAllRecebeByCNPJ (String cnpj) throws SQLException {
-		String query = String.format("SELECT * FROM RecebeDB WHERE CNPJ = '%s'", cnpj);
+		String query = String.format("SELECT * FROM %s WHERE RecebeDB.CNPJ = '%s'", tableJoin, cnpj);
 		return super.getAll(query);
 	}
 	
 	public List<Recebe> getAllRecebeByPeriod (Date intervaloInicio, Date intervaloFim) throws SQLException {
-		String query = String.format("SELECT * FROM RecebeDB WHERE DataInicio >= '%s' AND DataInicio <= '%s'", intervaloInicio, intervaloFim);
+		String query = String.format("SELECT * FROM %s WHERE DataInicio >= '%s' AND DataInicio <= '%s'", tableJoin, intervaloInicio, intervaloFim);
 		return super.getAll(query);
 	}
 	
 	public List<Recebe> getAll() throws SQLException {
-		String query = String.format("SELECT * FROM RecebeDB");
+		String query = String.format("SELECT * FROM %s", tableJoin);
 		return super.getAll(query);
 	}
 	
 	@Override
 	public Recebe instantiateObject(ResultSet resultSet) throws SQLException {
 		Recebe recebe = new Recebe();
-		recebe.setCarro(CarroDAO.getInstance().getCarroByRenavam(resultSet.getString("Renavam")));
-		recebe.setServico(ServicoDAO.getInstance().getServicoByID(resultSet.getInt("IdServico")));
+		recebe.setCarro(CarroDAO.getInstance().instantiateObject(resultSet));
+		recebe.setServico(ServicoDAO.getInstance().instantiateObject(resultSet));
 		recebe.setPrestadorServico(
-				PrestadorServicoDAO.getInstance().getPrestadorServicoByCNPJ(resultSet.getString("CNPJ")));
+				PrestadorServicoDAO.getInstance().instantiateObject(resultSet));
 		recebe.setDataInicio(resultSet.getDate("DataInicio"));
 		recebe.setDataFim(resultSet.getDate("DataFim"));
 		return recebe;
